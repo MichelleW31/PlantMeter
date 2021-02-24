2a509e335e0415bcf3597b41bb8289da49ba3df0
Michelle <mattie.williams31@gmail.com>
2021-02-24 23:42:28 +0000

    get react app servedgit add -A

--- routes.js
+++ routes.js
@@ -1,11 +1,14 @@
-const express = require('express')
+const express = require('express');
 // const pool = require('./pg-connection-pool.js').pool
-const app = express()
+const app = express();
+const path = require("path");
 
 app.get('/', function (req, res) {
-    console.log("get call has been made")
+    res.sendFile(path.join(__dirname, "build", "index.html"));
 })
 
-app.post('/av_vera' function(req, res) {
-    
+app.post('/av_vera', function (req, res) {
+    console.log("post call has been made");
 })
+
+module.exports = app;
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 //Requires
-import express, { static } from 'express';
+const express = require('express');
 const app = express();
 const port = 8081;
 const routes = require('./routes.js');
+const path = require("path");
 
 //Static Routes
-app.use(('/')), routes;
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(routes);
 
 //Run Server
 app.listen(process.env.PORT || port, () => console.log(`Listening intently on port ${port}`));
