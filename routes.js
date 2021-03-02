9da10b712a217730379744fa1fd16e9d35a39622
Michelle <mattie.williams31@gmail.com>
2021-03-02 00:45:35 +0000

    update routes and nodemon

--- routes.js
+++ routes.js
@@ -1,14 +1,21 @@
 const express = require('express');
-// const pool = require('./pg-connection-pool.js').pool
 const app = express();
 const path = require("path");
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database('./database/sensordata.db')
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname, "build", "index.html"));
 })
 
-app.post('/av_vera', function (req, res) {
+// app.get('/plants', function () {
+
+// })
+
+app.put('/plants:id', function (req, res) {
+    let param = req.params
     console.log("post call has been made");
+    console.log('this is the param', param);
 })
 
 module.exports = app;
