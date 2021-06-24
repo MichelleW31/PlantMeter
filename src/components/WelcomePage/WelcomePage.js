9524a664f0ea8db990c2e90fca3371dbe3685ed7
Michelle Williams <mattie.williams31@gmail.com>
2021-06-24 22:14:50 +0100

    adding homepage content and css

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -1,16 +1,15 @@
 import React from "react";
 import styles from "./WelcomePage.module.scss";
+import DripOnline from "../../assets/logo_blue.png";
 
 const WelcomePage = () => {
   return (
     <main className={styles.HomePageContainer}>
-      <h1 className={styles.copy}>Modrip</h1>
-      <h2>For drip on the go</h2>
-
       <section>
-        
+        <h1 className={styles.headerCopy}>Modrip</h1>
+        <h2 className={styles.subHeaderCopy}>For drip on the go</h2>
       </section>
-
+      <img className={styles.dripOnline} src={DripOnline} />
     </main>
   );
 };
