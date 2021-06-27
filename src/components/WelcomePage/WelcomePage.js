24fb802b7dee11ea1183fda630cad048b8c477eb
Michelle Williams <mattie.williams31@gmail.com>
2021-06-27 02:08:00 +0100

    fix up css

--- src/components/WelcomePage/WelcomePage.js
+++ src/components/WelcomePage/WelcomePage.js
@@ -4,12 +4,14 @@ import DripOnline from "../../assets/logo_blue.png";
 
 const WelcomePage = () => {
   return (
-    <main className={styles.HomePageContainer}>
-      <section>
+    <main className={styles.WelcomePageContainer}>
+      <section className={styles.copyContainer}>
         <h1 className={styles.headerCopy}>Modrip</h1>
         <h2 className={styles.subHeaderCopy}>For drip on the go</h2>
       </section>
-      <img className={styles.dripOnline} src={DripOnline} />
+      <section className={styles.imageContainer}>
+        <img className={styles.dripOnline} src={DripOnline} />
+      </section>
     </main>
   );
 };
